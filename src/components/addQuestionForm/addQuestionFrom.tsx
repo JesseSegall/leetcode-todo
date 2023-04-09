b09678ea1ca4679b20b7da60a077cbbdf0ddc911
Jesse <jsegall7@gmail.com>
2023-04-09 16:42:37 -0400

    updates for adding question to db and mutating

--- src/components/addQuestionForm/addQuestionFrom.tsx
+++ src/components/addQuestionForm/addQuestionFrom.tsx
@@ -1,5 +1,13 @@
-import React, { FC } from 'react';
-import { Box, Typography, Stack } from '@mui/material';
+import React, { FC, useState, ReactElement, useEffect } from 'react';
+import {
+  Box,
+  Typography,
+  Stack,
+  LinearProgress,
+  Button,
+  Alert,
+  AlertTitle,
+} from '@mui/material';
 
 import { QuestionTitleField } from './_questionTitleField';
 import { QuestionDescriptionField } from './_questionDescriptionField';
@@ -7,8 +15,55 @@ import { QuestionReviewDate } from './_questionReviewDate';
 import { QuestionSelectField } from './_questionSelectField';
 import { Status } from './enums/Status';
 import { Difficulty } from './enums/Difficulty';
+import { useMutation } from '@tanstack/react-query';
+import { sendApiRequest } from '../../helpers/sendApiRequest';
+import { ICreateQuestion } from '../taskArea/interfaces/ICreateQuestion';
 
-export const AddQuestionForm: FC = () => {
+export const AddQuestionForm: FC = (): ReactElement => {
+  // Declaring States
+
+  const [title, setTitle] = useState<string | undefined>(undefined);
+  const [description, setDescription] = useState<string | undefined>(undefined);
+  const [date, setDate] = useState<Date | null>(new Date());
+  const [status, setStatus] = useState<string>(Status.todo);
+  const [difficulty, setDifficulty] = useState<string>(Difficulty.easy);
+  const [showSuccess, setShowSuccess] = useState<boolean>(false);
+
+  // Create question mutation
+
+  const createQuestionMutation = useMutation((data: ICreateQuestion) =>
+    sendApiRequest('http://localhost:3200/questions', 'POST', data),
+  );
+
+  function createQuestionHandler() {
+    if (!title || !date || !description) {
+      return;
+    }
+
+    const question: ICreateQuestion = {
+      title,
+      description,
+      status,
+      date: date.toString(),
+      difficulty,
+    };
+    createQuestionMutation.mutate(question);
+  }
+
+  useEffect(() => {
+    if (createQuestionMutation.isSuccess) {
+      setShowSuccess(true);
+    }
+
+    const successTimeout = setTimeout(() => {
+      setShowSuccess(false);
+    }, 500);
+
+    return () => {
+      clearTimeout(successTimeout);
+    };
+  }, [createQuestionMutation.isSuccess]);
+
   return (
     <Box
       display="flex"
@@ -18,18 +73,37 @@ export const AddQuestionForm: FC = () => {
       px={4}
       my={6}
     >
+      {showSuccess && (
+        <Alert severity="success" sx={{ width: '100%', marginBottom: '16px' }}>
+          <AlertTitle>Success!</AlertTitle>
+          Question has been added!
+        </Alert>
+      )}
+
       <Typography mb={2} component="h2" variant="h6">
         Add A Question
       </Typography>
 
       <Stack sx={{ width: '100%' }} spacing={2}>
-        <QuestionTitleField />
-        <QuestionDescriptionField />
-        <QuestionReviewDate />
+        <QuestionTitleField
+          onChange={(e) => setTitle(e.target.value)}
+          disabled={createQuestionMutation.isLoading}
+        />
+        <QuestionDescriptionField
+          onChange={(e) => setDescription(e.target.value)}
+          disabled={createQuestionMutation.isLoading}
+        />
+        <QuestionReviewDate
+          value={date}
+          onChange={(date) => setDate(date)}
+          disabled={createQuestionMutation.isLoading}
+        />
         <Stack sx={{ width: '100%' }} direction="row" spacing={2}>
           <QuestionSelectField
             label="Status"
             name="Status"
+            value={status}
+            onChange={(e) => setStatus(e.target.value as string)}
             items={[
               {
                 value: Status.todo,
@@ -44,10 +118,13 @@ export const AddQuestionForm: FC = () => {
                 label: Status.completed.toUpperCase(),
               },
             ]}
+            disabled={createQuestionMutation.isLoading}
           />
           <QuestionSelectField
             label="Difficulty"
             name="Difficulty"
+            value={difficulty}
+            onChange={(e) => setDifficulty(e.target.value as string)}
             items={[
               {
                 value: Difficulty.easy,
@@ -62,8 +139,21 @@ export const AddQuestionForm: FC = () => {
                 label: Difficulty.hard.toLocaleUpperCase(),
               },
             ]}
+            disabled={createQuestionMutation.isLoading}
           />
         </Stack>
+        {/* Only loads prog bar if mutation func fires */}
+        {createQuestionMutation.isLoading && <LinearProgress />}
+
+        <Button
+          disabled={!title || !description || !date || !status || !difficulty}
+          onClick={createQuestionHandler}
+          variant="contained"
+          size="large"
+          fullWidth
+        >
+          Add a Question
+        </Button>
       </Stack>
     </Box>
   );
